7fcf8f445429e228777abea4d475c0b7f9cb2da7
佩宜 蔡 <iris631509@hotmail.com>
2019-05-15 16:16:49 +0800

    同步2

--- CjcuUniversity/Models/Enrollment.cs
+++ CjcuUniversity/Models/Enrollment.cs
@@ -16,6 +16,7 @@ namespace CjcuUniversity.Models
         public int CourseID { get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
+ 
 
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
--- CjcuUniversity/Models/Student.cs
+++ CjcuUniversity/Models/Student.cs
@@ -13,5 +13,7 @@ namespace CjcuUniversity.Models
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+      
     }
+
 }
